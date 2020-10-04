0716174dbac46a088ceacbdca1b1003d2fff0cb3
Dan McCandless <dkmccandless@gmail.com>
2020-10-05 02:23:44 +0800

    make Candidates communicate only with Groups
    Each Candidate sends its result to its Groups and does not communicate
    with adjacent Candidates. Each Group tracks the information received
    from its Candidates and sends one value to all of its Candidates under
    whichever of the following conditions occurs first: if a Candidate
    reports that it is true, the Group sends false to all of its
    Candidates; or, if all Candidates but one report that they are false,
    it sends true to all Candidates. Each Candidate acts only on the first
    value it receives and ignores subsequent values. (In a well-formed
    sudoku puzzle with a single solution, this is not a race condition
    because the truth value of a Candidate inference does not depend on the
    order of the logical deductions that prove it.)
    
    Therefore, each Candidate receives at most five values on its channel,
    and each Group precisely nine on its. Candidates do not need to send to
    each other, so they do not require knowledge of each other's adjacency.
    
    remove ach
    
    remove isAdjacent
    
    clarify doc comments
    
    declare nGroups globally
    
    factor out sendAll
    
    rename cch, gch, and b fields to cans, groups, and value

--- sudokami.go
+++ sudokami.go
@@ -3,9 +3,9 @@ Command sudokami solves sudoku puzzles through concurrent deduction.
 
 It launches a goroutine for each candidate inference in the puzzle to listen on a channel for deductions made by
 the inferences in the same cell or of the same digit in the same row, column, or 3x3 box (with which it is mutually exclusive).
-When a candidate deduces its necessity or falsehood, it informs the candidates adjacent to it or the groups it belongs to, respectively.
-When a group determines that exactly one of its candidates can be true, it informs its candidates.
-In this way, command sudokami can solve any puzzle that depends only on single candidate inference patterns.
+When a candidate deduces its necessity or falsehood, it informs the groups it belongs to.
+When a group determines that one of its candidates is true, it informs its candidates.
+In this way, command sudokami can solve any valid puzzle that depends only on single candidate inference patterns.
 
 Usage:
 Pass the puzzle as a string argument on the command line:
@@ -29,7 +29,13 @@ const (
 	m = 3
 
 	// d is the number of cells spanning the puzzle grid, and the number of digits used in the puzzle.
+	// It is also the number of Candidates in each Group.
 	d = m * m
+
+	// nGroups is the number of Groups a Candidate belongs to.
+	// Each Candidate represents a unique combination of one row, one column, and one digit.
+	// In addition, each Candidate belongs to one box.
+	nGroups = 4
 )
 
 func main() {
@@ -43,7 +49,7 @@ func main() {
 	g := NewGrid(&wg)
 	for i, n := range s {
 		if n != 0 {
-			g.Clue(i/9, i%9, n-1)
+			g.Clue(i/d, i%d, n-1)
 		}
 	}
 	wg.Wait()
@@ -52,60 +58,34 @@ func main() {
 
 // Candidate is an inference regarding a single candidate digit in a single cell.
 type Candidate struct {
-	// ch is used to receive information about the Candidate deduced by its Groups and adjacent Candidates.
+	// ch is used to receive information about the Candidate deduced by its Groups.
 	// The first value the Candidate receives on ch informs it whether its inference is in the puzzle's solution.
 	ch chan bool
 
-	// ach holds the channels of adjacent Candidates.
-	// If the Candidate's inference is true, it sends false to all adjacent Candidates.
-	ach []chan bool
+	// groups holds the channels of the Candidate's Groups.
+	// When the Candidate determines the truth value of its inference, it sends this value to each of its Groups.
+	groups []chan bool
 
-	// gch holds the channels of the Groups the Candidate belongs to.
-	// If the Candidate's inference is false, it sends false to all of its Groups.
-	gch []chan bool
-
-	// b records whether the Candidate has determined that its inference is true.
-	b bool
+	// value records the first value received on ch.
+	value bool
 }
 
 // NewCandidate creates a new Candidate, launches a goroutine for it to receive and send values, and returns a pointer to it.
 func NewCandidate(wg *sync.WaitGroup) *Candidate {
-	// nAdj is the number of adjacent Candidates.
-	// A Candidate shares its cell with m^2-1 other digits,
-	// its row with m^2-1 occurrences of the same digit in different columns,
-	// and its column with m^2-1 occurrences of the same digit in different rows.
-	// Besides these, there are (m-1)^2 other occurrences of the same digit in a different row and column of the same box.
-	const nAdj = 3*(m*m-1) + (m-1)*(m-1)
-
-	// nGroup is the number of Groups a Candidate belongs to.
-	// Each Candidate belongs to a unique combination of one row, one column, and one digit.
-	// In addition, each Candidate belongs to one box.
-	const nGroup = 4
-
 	c := &Candidate{
-		// Buffer ch with enough capacity to receive one value from NewGrid and one from
-		// each Group and adjacent Candidate so that channel operations will never block.
-		ch:  make(chan bool, nAdj+nGroup+1),
-		ach: make([]chan bool, 0, nAdj),
-		gch: make([]chan bool, 0, nGroup),
+		// ch will receive one value from each Group and possibly one value from NewGrid.
+		// Buffer it with sufficient capacity so that sends on it never block.
+		ch:     make(chan bool, nGroups+1),
+		groups: make([]chan bool, 0, nGroups),
 	}
 
 	// Listen on ch. The first value received indicates the truth value of c's inference.
-	// If true is received, send false to all adjacent Candidates; if false is received, inform the Groups.
-	// In either case, call wg.Done and return: since ch is buffered to sufficient capacity,
-	// it is not necessary to consume the remaining values sent.
+	// Report this value to c's Groups, then call wg.Done and return: since ch is buffered
+	// to sufficient capacity, it is not necessary to consume the remaining values sent.
 	wg.Add(1)
 	go func() {
-		switch c.b = <-c.ch; c.b {
-		case true:
-			for _, a := range c.ach {
-				a <- false
-			}
-		case false:
-			for _, g := range c.gch {
-				g <- false
-			}
-		}
+		c.value = <-c.ch
+		sendAll(c.groups, c.value)
 		wg.Done()
 	}()
 
@@ -117,13 +97,13 @@ func NewCandidate(wg *sync.WaitGroup) *Candidate {
 // precisely one of which is true in a puzzle's solution.
 type Group struct {
 	// ch is used to receive the deductions of the Group's Candidates.
-	// Each Candidate that determines that its inference is false sends false once on ch.
+	// When each Candidate determines the truth value of its inference, it sends this value on ch.
 	ch chan bool
 
-	// cch holds the channels of the Group's Cardidates.
-	// When the Group has determined that only one of its Candidates can be true,
-	// it sends true to all of its Candidates.
-	cch []chan bool
+	// cans holds the channels of the Group's Cardidates.
+	// When the Group determines the truth value of the Candidate(s) yet to report to it,
+	// it sends this value to all of its Candidates.
+	cans []chan bool
 
 	// n is the number of Candidate inferences that have not been determined to be false.
 	n int
@@ -135,22 +115,26 @@ func NewGroup() *Group {
 	const nCan = d
 
 	g := &Group{
-		// Each of g's Candidates will send one value when it determines that it is false.
-		ch:  make(chan bool, nCan-1),
-		cch: make([]chan bool, 0, nCan),
-		n:   nCan,
+		// ch will receive one value from each Candidate.
+		// Buffer it with sufficient capacity so that sends on it never block.
+		ch:   make(chan bool, nCan),
+		cans: make([]chan bool, 0, nCan),
+		n:    nCan,
 	}
 
 	// Listen on ch. When a Candidate indicates that it is false, decrement n.
-	// When n reaches 1, g has received complete information: send true to all Candidates and return.
+	// Continue until one of the following conditions is fulfilled:
+	// 1. If a Candidate indicates that it is true, all others must be false: send false to all Candidates and return.
+	// 2. If n reaches 1, the remaining Candidate must be true: send true to all Candidates and return.
 	go func() {
 		for g.n > 1 {
-			<-g.ch
+			if <-g.ch {
+				sendAll(g.cans, false)
+				return
+			}
 			g.n--
 		}
-		for _, c := range g.cch {
-			c <- true
-		}
+		sendAll(g.cans, true)
 	}()
 
 	return g
@@ -165,8 +149,8 @@ type Grid struct {
 // NewGrid creates a new Grid, creates and connects its Candidates and Groups, and returns a pointer to it.
 func NewGrid(wg *sync.WaitGroup) *Grid {
 	g := &Grid{
-		cs: make([]*Candidate, d*d*d), // row * d^2 + column * d + digit
-		gs: make([]*Group, 4*d*d),     // row-column, row-digit, column-digit, box-digit
+		cs: make([]*Candidate, d*d*d),   // row * d^2 + column * d + digit
+		gs: make([]*Group, nGroups*d*d), // row-column, row-digit, column-digit, box-digit
 	}
 	for i := range g.cs {
 		g.cs[i] = NewCandidate(wg)
@@ -176,28 +160,20 @@ func NewGrid(wg *sync.WaitGroup) *Grid {
 	}
 	for i, ci := range g.cs {
 		connect := func(gr *Group) {
-			ci.gch = append(ci.gch, gr.ch)
-			gr.cch = append(gr.cch, ci.ch)
+			ci.groups = append(ci.groups, gr.ch)
+			gr.cans = append(gr.cans, ci.ch)
 		}
 		connect(g.gs[0*d*d+row(i)*d+col(i)])
 		connect(g.gs[1*d*d+row(i)*d+dig(i)])
 		connect(g.gs[2*d*d+col(i)*d+dig(i)])
 		connect(g.gs[3*d*d+box(i)*d+dig(i)])
-
-		for j, cj := range g.cs {
-			if !isAdjacent(i, j) {
-				continue
-			}
-			// Add cj's channel to ci's list
-			ci.ach = append(ci.ach, cj.ch)
-		}
 	}
 
 	return g
 }
 
 // Clue records that the cell in the given row and column contains the given digit,
-// with all parameters in the range [0, 8].
+// with all parameters in the range [0, d-1].
 func (g *Grid) Clue(row, column, digit int) { g.cs[index(row, column, digit)].ch <- true }
 
 // String returns a string representation of g.
@@ -207,7 +183,7 @@ func (g *Grid) String() string {
 		for c := 0; c < d; c++ {
 			var n, count int
 			for i := 0; i < d; i++ {
-				if g.cs[index(r, c, i)].b {
+				if g.cs[index(r, c, i)].value {
 					count++
 					n = i
 				}
@@ -223,6 +199,13 @@ func (g *Grid) String() string {
 	return s
 }
 
+// sendAll sends b on all channels in chans.
+func sendAll(chans []chan bool, b bool) {
+	for _, ch := range chans {
+		ch <- b
+	}
+}
+
 // index returns a Candidate's index.
 func index(row, column, digit int) int { return row*d*d + column*d + digit }
 
@@ -236,18 +219,6 @@ func box(n int) int {
 	// ---+---+---
 	//  6 | 7 | 8
 	return row(n)/m*m + col(n)/m
-}
-
-// isAdjacent reports whether i and j index adjacent Candidates.
-// Candidates are adjacent if they are distinct and have the same row and column, row and digit, column and digit, or box and digit.
-func isAdjacent(i, j int) bool {
-	if i == j {
-		return false
-	}
-	if dig(i) != dig(j) {
-		return row(i) == row(j) && col(i) == col(j)
-	}
-	return row(i) == row(j) || col(i) == col(j) || box(i) == box(j)
 }
 
 // parseInput parses an input string.
